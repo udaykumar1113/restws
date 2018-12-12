59218bd449cf88a0b085f015ee428e9e1492fee9
udayk <udaykumar1113@gmail.com>
2018-12-12 17:33:38 -0500

    Added exception mapper to send custom JSON messages to client

--- src/main/java/com/uday/restws/service/PatientExceptionMapper.java
+++ src/main/java/com/uday/restws/service/PatientExceptionMapper.java
@@ -2,6 +2,7 @@ package com.uday.restws.service;
 
 import com.uday.restws.exception.NoPatientException;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -10,6 +11,13 @@ import javax.ws.rs.ext.Provider;
 public class PatientExceptionMapper implements ExceptionMapper<NoPatientException> {
     @Override
     public Response toResponse(NoPatientException e) {
-        return Response.status(404).build();
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append("{");
+        stringBuilder.append("\"status\":\"error\"");
+        stringBuilder.append(",");
+        stringBuilder.append("\"message\":\"Record not found\"");
+        stringBuilder.append("}");
+        return Response.serverError().entity(stringBuilder.toString())
+                .type(MediaType.APPLICATION_JSON_TYPE).build();
     }
 }
